761fe454afe4651047a680d17b8c84a649f8ba08
johan162 <johan162@8976e5d5-1644-42d6-8671-f5d7455f965a>
2010-11-25 22:54:19 +0000

    Removed bootstrap and added mkcl.sh instead

--- trunk/mkcl.sh
+++ trunk/mkcl.sh
@@ -1,11 +1,6 @@
 #!/bin/sh
-# $Id$
-# Bootstrap sequence to create initial build environment
-aclocal
-autoheader
-autoconf
+#$Id$
+#Create ChangeLog from subversion logmessages
 if test -d .svn; then
     svn log --xml -v -rHEAD:1 | xsltproc --stringparam strip-prefix "trunk/" scripts/svn2cl.xsl - > ChangeLog
 fi
-automake -a -c
-
