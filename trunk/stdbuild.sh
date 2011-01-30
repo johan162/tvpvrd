a04c2d44c84d915a599afe8d9c6061ba596ecc82
johan162 <johan162@8976e5d5-1644-42d6-8671-f5d7455f965a>
2011-01-30 12:27:23 +0000

    Bumped version to 3.0.0, updated docs

--- trunk/stdbuild.sh
+++ trunk/stdbuild.sh
@@ -3,7 +3,7 @@
 # Utility script to do a standard build
 ./stdconfig.sh
 if test "$?" = 0; then
-    make CFLAGS="-O1" -j4 -s
+    make -j4 -s
 else
     echo "Configuration failed. Cannot build package."
 fi
