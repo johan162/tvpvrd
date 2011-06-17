34b86a97f8483602bb237579d8022e6518fc6d3c
johan162 <johan162@8976e5d5-1644-42d6-8671-f5d7455f965a>
2011-06-17 21:33:22 +0000

    Tagged 3.2.1

---
+++ tags/3.2.1/src/transc.h
@@ -0,0 +1,505 @@
+/* =========================================================================
+ * File:        TRANSC.H
+ * Description: Module to handle transcoding profiles
+ * Author:      Johan Persson (johan162@gmail.com)
+ * SVN:         $Id$
+ *
+ * Copyright (C) 2009 Johan Persson
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>
+ * =========================================================================
+ */
+
+#ifndef _TRANSC_H
+#define	_TRANSC_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+/*
+ * DEFAULT_TRANSCODING_PROFILE string
+ * Defailt transcoding profile that all recording will use if not otherwise
+ * specified
+ */
+#define DEFAULT_TRANSCODING_PROFILE "normal"
+
+/*----------------------------------------------------------------------------------
+ * The following defines specfies the default values used for a transcoding
+ * profile when the value is not specified in the profile fille.
+ *----------------------------------------------------------------------------------
+ */
+
+/*
+ * DEFAULT_USE_TRANSCODING boolean
+ * Should we use transcoding at all
+ */
+#define DEFAULT_USE_TRANSCODING 1
+
+/*
+ * DEFAULT_VCODEC string
+ * What video codec ffmpeg will use by default
+ */
+#define DEFAULT_PROFILE_VCODEC "libx264"
+
+/*
+ * DEFAULT_VPRE string
+ * Default preset for the -vpre setting for ffmpeg
+ */
+#define DEFAULT_PROFILE_VPRE "normal"
+
+/*
+ * DEFAULT_PROFILE_VIDEO_BITRATE integre
+ * Default video bitrate in kbps
+ */
+#define DEFAULT_PROFILE_VIDEO_BITRATE 600
+
+/*
+ * DEFAULT_PROFILE_VIDEO_PEAK_BITRATE integer
+ * Default video peak bitrate in kbps
+ */
+#define DEFAULT_PROFILE_VIDEO_PEAK_BITRATE  800
+
+/*
+ * DEFAULT_PROFILE_AUDIO_BITRATE integer
+ * Default audio bitrate in kbps
+ */
+#define DEFAULT_PROFILE_AUDIO_BITRATE 192
+
+/*
+ * DEFAULT_PROFILE_PASS integer
+ * Default number of passes 1 or 2
+ */
+#define DEFAULT_PROFILE_PASS 2
+
+/*
+ * DEFAULT_PROFILE_VIDEO_SIZE integer
+ * Default video size
+ * The empty string indicates that the size should be the same
+ * as the input size (from the MP2 stream).
+ * The size is otherwise given as a string "WxH". Shortcuts can also be
+ * used, e.g. sqcif, qcif, cif, 4cif, qqvga, qvga, vga, svga etc.
+ * See the man page for ffmeg for more details
+ */
+#define DEFAULT_PROFILE_VIDEO_SIZE ""
+
+/*
+ * DEFAULT_CROP_TOP integer
+ * DEFAULT_CROP_BOTTOM integer
+ * DEFAULT_CROP_LEFT integer
+ * DEFAULT_CROP_RIGHT integer
+ * Default cropping size for video if nothing else specified
+ */
+#define DEFAULT_PROFILE_CROP_TOP 0
+#define DEFAULT_PROFILE_CROP_BOTTOM 0
+#define DEFAULT_PROFILE_CROP_LEFT 0
+#define DEFAULT_PROFILE_CROP_RIGHT 0
+
+/*
+ * DEFAULT_PROFILE_AUDIO_CODEC string
+ * If left empty ffmpeg will default to using the aac encoder.
+ * Any other installed libraries can be used. For example to use the
+ * lame mp3 encoder use "libmp3lame" as value
+ */
+#define DEFAULT_PROFILE_ACODEC ""
+
+/*
+ * DEFAULT_PROFILE_FILE_EXTENSION string
+ * The fiel extension to be used on the transcoded file
+ */
+#define DEFAULT_PROFILE_FILE_EXTENSION ".mp4"
+/*
+ * DEFAULT_PROFILE_EXTRA_FFMPEG_OPTIONS string
+ * User selectable additoinal options to use for ffmpeg
+ * This option string is added to the end of the ffmpeg command
+ * just before the output file is specified
+ */
+#define DEFAULT_PROFILE_EXTRA_FFMPEG_OPTIONS ""
+
+// The name of the sections in an ini file for a profile
+#define FFMPEG_SECTION "ffmpeg"
+#define ENCODER_SECTION "encoder"
+
+
+/*
+ * DEFAULT_VIDEO_BITRATE integer
+ * DEFAULT_VIDEO_PEAK_BITRATE integer
+ * Deafult video bitrates 3 Mbps , peak 3.5 Mbps
+ * Values should be in range 500000 up to 8000000
+ */
+#define DEFAULT_VIDEO_BITRATE 3000000
+#define DEFAULT_VIDEO_PEAK_BITRATE 3500000
+
+/*
+ * DEFAULT_VIDEO_FRAME_SIZE string
+ * Default MP2 frame size 3-quarter
+ * Supported format at present are
+ *  "default = 720,576
+ *  "qvga = 320,240
+ *  "qqvga" = 160,120
+ *  "vga" = 640,480
+ *  "cif" = 352,288
+ *  "3q" = 480,384
+ *  "3qmp4" = 480,352
+ *   "half" = 360,288
+ */
+#define DEFAULT_VIDEO_FRAME_SIZE "3qmp4"
+
+/*
+ * Deafult audio sampling freq
+ */
+#define DEFAULT_AUDIO_SAMPLING V4L2_MPEG_AUDIO_SAMPLING_FREQ_44100
+
+/*
+ * Default audio bitrate for MPEG Layer 2
+ */
+#define DEFAULT_AUDIO_BITRATE V4L2_MPEG_AUDIO_L2_BITRATE_256K
+
+/*
+ * Default video aspect rate
+ * A possible value would also be V4L2_MPEG_VIDEO_ASPECT_4x3
+ */
+#define DEFAULT_VIDEO_ASPECT V4L2_MPEG_VIDEO_ASPECT_16x9
+
+/*
+ * KEEP_MP2FILE bool
+ * Keep original MP2 file from video card even after transcoding
+ */
+#define KEEP_MP2FILE 1
+
+
+/**
+  * Holds all the information on a specific transcoding profile. The profile
+ * has two main sections.
+ * 1) The setting for the HW encoder on the capture card
+ * 2) The ffmpeg settings to use at the actual transcoding
+ */
+struct transcoding_profile_entry {
+    
+    /* profile name */
+    char name[32];
+    
+    /* HW encoder entries */
+    unsigned encoder_keep_mp2file;
+    unsigned encoder_video_bitrate;
+    unsigned encoder_video_peak_bitrate;
+    unsigned encoder_audio_bitrate;
+    unsigned encoder_audio_sampling;
+    unsigned encoder_video_aspect;
+    char encoder_video_frame_size_name[32];
+    
+    /* ffmpeg entries */
+    unsigned use_transcoding;
+    unsigned video_bitrate;
+    unsigned video_peak_bitrate;
+    unsigned pass;
+    unsigned audio_bitrate;
+    char size[32];
+    char vpre[32];
+    char vcodec[32];
+    char acodec[32];
+    char file_extension[16];
+    unsigned crop_top,crop_bottom;
+    unsigned crop_left,crop_right;
+    char extra_ffmpeg_options[256];
+    char filename[256];
+};
+
+/**
+ * MAX_TRANS_PROFILES integer
+ * The maximum number of allowed ffmpeg profiles
+ */
+#define MAX_TRANS_PROFILES 64
+
+// Wee keep all ongoing transcoding in an array so that we now what is going on.
+// Each transcoding is run as a separate process and that process is
+// monitored by a thread in the main server.
+struct ongoing_transcoding {
+    time_t start_ts;
+    char *workingdir;
+    char *filename;
+    char *cmd;
+    struct transcoding_profile_entry *profile;
+    pid_t pid;
+};
+extern struct ongoing_transcoding *ongoing_transcodings[] ;
+extern const size_t max_ongoing_transcoding;
+
+// We keep track on all transcodings that are waiting to happen
+#define MAX_WAITING_TRANSCODINGS 64
+struct waiting_transcoding_t {
+    char filename[255];
+    char profilename[255];
+    time_t timestamp;
+};
+extern struct waiting_transcoding_t wtrans[] ;
+
+
+/**
+ * Check if ffmpeg binaries can be found at the specified location
+ * @return -1 on failure, 0 on success
+ */
+int
+check_ffmpeg_bin(void);
+
+/**
+ * Remember an ongoing transcoding
+ * @param workingdir
+ * @param short_filename
+ * @param cmd_ffmpeg
+ * @param profile
+ * @param pid
+ * @return An index that uniqely identifies this ongoing transcoding
+ */
+int
+record_ongoingtranscoding(char *workingdir,char *short_filename,char *cmd_ffmpeg,
+                          struct transcoding_profile_entry *profile, pid_t pid);
+
+/**
+ * Forget an ongoing transcoding
+ * @param idx The index of the transcoding to forget.
+ */
+void
+forget_ongoingtranscoding(int idx);
+
+/**
+ * Store a string represenattion of all ongoing transcodings in the specified buffer
+ * @param obuff Output buffer
+ * @param size Maximum size of buffer
+ * @param show_ffmpegcmd 1=Alos show the details on the ffmpeg command used
+ * @return -1 failure, 0 on success
+ */
+int
+list_ongoing_transcodings(char *obuff, size_t size, int show_ffmpegcmd);
+
+/**
+ * read all transcoding profiles from file
+ * @return -1 on failure, 0 on success
+ */
+int
+read_transcoding_profiles(void);
+
+/**
+ * Get the details fro the specified transcoding profile name
+ * @param name profile name
+ * @param entry a pointer to a static buffer with the transcoding information
+ */
+void
+get_transcoding_profile(char *name, struct transcoding_profile_entry **entry);
+
+/**
+ * Wait until load on the server is low enough to start another transcoding process
+ * @param filename
+ * @return -1 failure, 0 on success
+ */
+int
+wait_to_transcode(char *filename);
+
+/**
+ * Create the actual ffmpeg command line necessary according to the chosen profile
+ * @param filename
+ * @param profile
+ * @param destfile
+ * @param destsize
+ * @param cmd
+ * @param size
+ * @return -1 failure, 0 on success
+ */
+int
+create_ffmpeg_cmdline(char *filename, struct transcoding_profile_entry *profile,
+                      char *destfile, size_t destsize, char *cmd, size_t size);
+
+/**
+ * Get a list of all currently specified transcodings
+ * @param start
+ * @return number of entries in list
+ */
+unsigned
+get_transcoding_profile_list(struct transcoding_profile_entry **start[]);
+
+/**
+ * Return number of ongoing transcodings
+ * @return Number of currently ongoing transcodings
+ */
+size_t
+get_num_ongoing_transcodings(void);
+
+
+/**
+ * Check if the named transcoding proifile exists ot not
+ * @param name
+ * @return Boolean, 1 exists , 0 doesn't
+ */
+int
+transcoding_profile_exist(char *name);
+
+/**
+ * Re-read all transcoding profiles from file
+ */
+void
+refresh_transcoding_profiles(void);
+
+/**
+ * Dump s atring representation of all transcoding profiles to the specified buffer
+ * @param name
+ * @param buff
+ * @param size
+ * @return -1 failure, 0 on success
+ */
+int 
+dump_transcoding_profile(char *name, char *buff, size_t size);
+
+/**
+ * Kill_ongoing transcoding processes.
+ * Kill the specified ongoing transcoding
+ * @param idx
+ */
+int
+kill_ongoing_transcoding(int idx);
+
+/**
+ * Kill all ongoing transcoding processes. This can be called either
+ * via the kill command from a client or it can be automatically called
+ * when the server shuts down. This beahviour is constrolled via the
+ * configuration file.
+ *
+ */
+void
+kill_all_ongoing_transcodings(void);
+
+/**
+ * Transcode the specified file using the optional profilename
+ * If wait is 0 the transcoding will start immediately without checking
+ * for server load.
+ * @param filename
+ * @param profilename
+ * @return
+ */
+int
+transcode_file(char *filename, char *profilename);
+
+/**
+ * Read a list of filenames to transcode. The list of files are read from the
+ * named file. All files are encoded using the same profile.
+ * @param filename File with list of 
+ * @param profilename
+ * @return
+ */
+int
+read_transcode_filelist(char *filename, char *profilename);
+
+/**
+ * Transcode all videos in named directory
+ * @param dirpath
+ * @param profilename
+ * @return
+ */
+int
+transcode_whole_directory(char *dirpath, char *profilename);
+
+/**
+ * Return information on the currently queued files for transcoding from the chosen
+ * file list
+ * @param num The number of the filelist to show
+ * @param buffer String buffer where the informatoin will be written
+ * @param len Length of buffer
+ * @param incfiles 1=include files names in information
+ * @return -1 on error 0 otherwise
+ */
+int
+get_queued_transc_filelists_info(int num, char *buffer, size_t len, int incfiles);
+
+/**
+ * Return a list of all defined profiles
+ * @param buff
+ * @param maxlen
+  * @return -1 on error 0 otherwise
+ */
+int
+list_profile_names(char *buff, size_t maxlen);
+
+/**
+ * Return a list of all defined profiles with HTML links to display
+ * the details of the profile (assumes the Web-interface)
+ * @param buff
+ * @param maxlen
+ * @return -1 on error 0 otherwise
+ */
+int
+list_profile_names_htmllinks(char *buff, size_t maxlen);
+
+/**
+ * Store a list of pointers to profile names in the supplied buffer
+ * Note: The profile names cannot be modified in any way
+ * @param list
+ * @param maxlen
+ * @return number of names in list
+ */
+unsigned
+get_profile_names(const char *list[], size_t maxlen);
+
+
+/**
+ *
+ * @param short_filename
+ * @param profile_name
+ * @return
+ */
+int
+remember_waiting_transcoding(char *short_filename, char *profile_name);
+
+/**
+ *
+ * @param idx
+ * @return
+ */
+int
+forget_waiting_transcoding(int idx);
+
+/**
+ *
+ * @param buffer
+ * @param maxlen
+ * @return
+ */
+int
+list_waiting_transcodings(char *buffer, size_t maxlen);
+
+/**
+ * Transcode a specified MP2 file into MP4 using the specified profile. After
+ * transcoding is done move the file to the corresponding MP4 directory.
+ * The values returned as the last three parameters will be used to update
+ * the statistics by the calling function.
+ * @param datadir
+ * @param workingdir
+ * @param short_filename
+ * @param basename
+ * @param profile
+ * @param filesize
+ * @param transcode_time
+ * @param avg_5load
+ * @param updatedfilename
+ * @return
+ */
+int
+transcode_and_move_file(char *datadir, char *workingdir, char *short_filename, char *recurrence_title,
+                        struct transcoding_profile_entry *profile,
+                        unsigned *filesize, struct timeall *transcode_time, float *avg_5load, char *updatedfilename);
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* _TRANSC_H */
+
---
+++ tags/3.2.1/src/tvhistory.c
@@ -0,0 +1,435 @@
+/* =========================================================================
+ * File:        TVHISTORY.C
+ * Description: Module to handle history recording of made recordings
+ * Author:      Johan Persson (johan162@gmail.com)
+ * SVN:         $Id$
+ *
+ * Copyright (C) 2011 Johan Persson
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>
+ * =========================================================================
+ */
+
+// We want the full POSIX and C99 standard
+#define _GNU_SOURCE
+
+// And we need to have support for files over 2GB in size
+#define _LARGEFILE_SOURCE
+#define _LARGEFILE64_SOURCE
+#define _FILE_OFFSET_BITS 64
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <dirent.h>
+#include <signal.h>
+#include <sys/wait.h>
+#include <math.h>
+#include <syslog.h>
+#include <errno.h>
+#include <sys/param.h> // Needed to get MIN()/MAX()
+#include <time.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+// XML2 lib headers
+#include <libxml2/libxml/parser.h>
+#include <libxml2/libxml/tree.h>
+#include <libxml2/libxml/xmlreader.h>
+
+#include "tvhistory.h"
+#include "tvpvrd.h"
+#include "tvconfig.h"
+#include "utils.h"
+#include "xstr.h"
+#include "tvplog.h"
+#include "datetimeutil.h"
+#include "mailutil.h"
+
+/**
+ * Record for array of history records
+ */
+struct histrec {
+    char *title;
+    char *filepath;
+    time_t ts_start;
+    time_t ts_end;
+    char *profile;
+};
+static struct histrec history[HISTORY_LENGTH];
+static size_t nrecs = 0;
+
+/*
+Example history file
+ * 
+ <tvrechistory version="1">
+ <recording>
+ <title></title>
+ <start_ts></start_ts>
+ <end_ts></end_ts>
+ <filepath></filepath>
+ </recording>
+ </tvrechistory>
+ 
+ */
+
+#define XMLHISTDB_VERSIONNUM "1"
+
+static const xmlChar *xmldb_version = (xmlChar *) XMLHISTDB_VERSIONNUM;
+static const xmlChar *xmldb_nameText = (xmlChar *) "text";
+static const xmlChar *xmldb_nameVersion = (xmlChar *) "version";
+static const xmlChar *xmldb_root = (xmlChar *) "tvpvrdhistory";
+static const xmlChar *xmldb_nameRecording = (xmlChar *) "recording";
+static const xmlChar *xmldb_nameTitle = (xmlChar *) "title";
+static const xmlChar *xmldb_nameStart = (xmlChar *) "timestampstart";
+static const xmlChar *xmldb_nameEnd = (xmlChar *) "timestampend";
+static const xmlChar *xmldb_nameFilepath = (xmlChar *) "filepath";
+static const xmlChar *xmldb_nameProfile = (xmlChar *) "profile";
+
+static void
+processRecord(xmlNodePtr node) {
+    xmlNodePtr childnode;
+
+    node = node->xmlChildrenNode;
+    
+    while (node != NULL ) {
+        // Ignore text in between proper child tags
+        if ( xmlStrcmp(node->name, xmldb_nameText) ) {
+            childnode = node->xmlChildrenNode;
+            if (xmlStrcmp(node->name, xmldb_nameStart) == 0) {
+                if (childnode && xmlStrcmp(childnode->name, xmldb_nameText) == 0) {
+                    history[nrecs].ts_start = xatol((char *) childnode->content);
+                }
+            } else if (xmlStrcmp(node->name, xmldb_nameEnd) == 0) {
+                if (childnode && xmlStrcmp(childnode->name, xmldb_nameText) == 0) {
+                    history[nrecs].ts_end = xatol((char *) childnode->content);
+                }
+            } else if (xmlStrcmp(node->name, xmldb_nameTitle) == 0) {
+                if (childnode && xmlStrcmp(childnode->name, xmldb_nameText) == 0) {
+                    history[nrecs].title = strdup((char *) childnode->content);
+                }
+            } else if (xmlStrcmp(node->name, xmldb_nameFilepath) == 0) {
+                if (childnode && xmlStrcmp(childnode->name, xmldb_nameText) == 0) {
+                    history[nrecs].filepath = strdup((char *) childnode->content);
+                }
+            } else if (xmlStrcmp(node->name, xmldb_nameProfile) == 0) {
+                if (childnode && xmlStrcmp(childnode->name, xmldb_nameText) == 0) {
+                    history[nrecs].profile = strdup((char *) childnode->content);
+                }                
+            } else {
+                logmsg(LOG_NOTICE, "Unknown XML node name in history DB: %s", node->name);
+            }
+        }
+        node = node->next;
+    }
+}
+
+/**
+ * Read old history file from file
+ * @return 
+ */
+static int
+tvhist_read(void) {
+    char xmlhistfile[256];
+
+    snprintf(xmlhistfile, sizeof (xmlhistfile) - 1, "%s/xmldb/%s", datadir, HISTORYDB_FILENAME);
+    xmlhistfile[sizeof (xmlhistfile) - 1] = '\0';
+
+    xmlNodePtr node;
+    xmlDocPtr doc;
+    xmlChar *xmlver;
+
+    /*
+     * this initialize the library and check potential ABI mismatches
+     * between the version it was compiled for and the actual shared
+     * library used.
+     */
+    LIBXML_TEST_VERSION
+
+    // Parse the XML file
+    doc = xmlParseFile(xmlhistfile);
+    if (doc == NULL) {
+        logmsg(LOG_NOTICE, "Unable to read XML history file: '%s' ( %d : %s )", xmlhistfile, errno, strerror(errno));
+        return -1;
+    }
+
+    node = xmlDocGetRootElement(doc);
+
+    if (xmlStrcmp(node->name, xmldb_root)) {
+        logmsg(LOG_ERR, "XML file is not a proper history database file. Wrong root element. Found '%s' when expecting '%s'",
+                node->name, xmldb_root);
+        xmlFreeDoc(doc);
+        return -1;
+    }
+
+    // Check that the version of the file is the expected
+    xmlver = xmlGetProp(node, xmldb_nameVersion);
+    if (xmlStrcmp(xmlver, xmldb_version)) {
+        logmsg(LOG_NOTICE, "Expected XML history DB version '%s' but found version '%s'.", xmldb_version, xmlver);
+        if (xatoi((char *) xmlver) > xatoi((char *) xmldb_version)) {
+            logmsg(LOG_NOTICE, "Can not handle a newer history DB version. Please upgrade daaemon.");
+            xmlFreeDoc(doc);
+            return -1;
+        }
+    }
+    xmlFree(xmlver);
+    
+    nrecs=0;
+    node = node->xmlChildrenNode;
+    while (node != NULL) {
+        if (xmlStrcmp(node->name, xmldb_nameRecording) == 0) {
+            processRecord(node);
+            ++nrecs;
+            logmsg(LOG_DEBUG,"Read history title: %s",history[nrecs-1].title);
+        }
+        node = node->next;
+    }
+       
+    
+    xmlFreeDoc(doc);
+    xmlCleanupParser();
+
+    return 0;
+}
+
+/**
+ * Dump data structure to file
+ * @return 
+ */
+static int
+tvhist_write(void) {
+    char xmlhistfile[256];
+    int fd;
+
+    snprintf(xmlhistfile, sizeof (xmlhistfile) - 1, "%s/xmldb/%s", datadir, HISTORYDB_FILENAME);
+    xmlhistfile[sizeof (xmlhistfile) - 1] = '\0';
+    fd = open(xmlhistfile, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+    if (fd == -1) {
+        logmsg(LOG_ERR, "Can't open '%s' XML history file for writing. (%d : %s) ",
+                xmlhistfile, errno, strerror(errno));
+        return -1;
+    }
+
+    time_t now = time(NULL);
+
+    // Test if the first write was ok then we assume that the rest will
+    // be ok as well. Not perfect but at least this way we will catch
+    // wrong permissions
+
+    if (-1 == _writef(fd, "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n")) {
+        return -1;
+    }
+    _writef(fd, "<!-- Created: %s -->\n", ctime(&now));
+    _writef(fd, "<%s %s=\"%s\">\n", xmldb_root, xmldb_nameVersion, xmldb_version);
+
+    for (size_t i = 0; i < nrecs; ++i) {
+        _writef(fd, "  <%s>\n", xmldb_nameRecording);
+        _writef(fd, "    <%s>%s</%s>\n", xmldb_nameTitle, history[i].title, xmldb_nameTitle);
+        _writef(fd, "    <%s>%lld</%s>\n", xmldb_nameStart, (long long int)history[i].ts_start, xmldb_nameStart);
+        _writef(fd, "    <%s>%lld</%s>\n", xmldb_nameEnd, (long long int)history[i].ts_end, xmldb_nameEnd);
+        _writef(fd, "    <%s>%s</%s>\n", xmldb_nameFilepath, history[i].filepath, xmldb_nameFilepath);
+        _writef(fd, "    <%s>%s</%s>\n", xmldb_nameProfile, history[i].profile, xmldb_nameProfile);
+        _writef(fd, "  </%s>\n", xmldb_nameRecording);
+    }
+    _writef(fd, "</%s>\n", xmldb_root);
+    close(fd);
+    return 0;
+
+}
+
+static void
+tvhist_free(void) {
+    for (size_t idx = 0; idx < nrecs; ++idx) {
+        if (history[idx].filepath) {
+            free(history[idx].filepath);
+            history[idx].filepath = (char *) NULL;
+        }
+        if (history[idx].title) {
+            free(history[idx].title);
+            history[idx].title = (char *) NULL;
+        }
+        if (history[idx].profile) {
+            free(history[idx].profile);
+            history[idx].profile = (char *) NULL;
+        }        
+    }
+    nrecs = 0;
+}
+
+void
+hist_init(void) {
+    logmsg(LOG_DEBUG,"Calling hist_init()");
+    tvhist_free();
+    if (tvhist_read()) {
+        logmsg(LOG_NOTICE, "Failed to read old history file. Will create an empty new history file.");
+        tvhist_write();
+    } else {
+        logmsg(LOG_DEBUG, "Read history XML file.");
+    }
+}
+
+int
+hist_update(char *title, const time_t ts_start, const time_t ts_end,
+            char *fullPathFilename, char *profile) {
+
+    logmsg(LOG_DEBUG,"Adding history for: title=%s",title);
+    
+    // Shift all records down one slot and free the last record
+    if (nrecs == HISTORY_LENGTH) {
+        if (history[HISTORY_LENGTH - 1].filepath) {
+            free(history[HISTORY_LENGTH - 1].filepath);
+            history[HISTORY_LENGTH - 1].filepath = (char *) NULL;
+        }
+        if (history[HISTORY_LENGTH - 1].title) {
+            free(history[HISTORY_LENGTH - 1].title);
+            history[HISTORY_LENGTH - 1].title = (char *) NULL;
+        }
+        if (history[HISTORY_LENGTH - 1].profile) {
+            free(history[HISTORY_LENGTH - 1].profile);
+            history[HISTORY_LENGTH - 1].profile = (char *) NULL;
+        }        
+
+    }
+        
+    for (size_t i = HISTORY_LENGTH - 1; i > 0; i--) {
+        memcpy(&history[i], &history[i - 1], sizeof (struct histrec));
+    }
+
+    history[0].title = strdup(title);
+    history[0].profile = strdup(profile);
+    history[0].filepath = strdup(fullPathFilename);
+    history[0].ts_start = ts_start;
+    history[0].ts_end = ts_end;
+
+    if( nrecs < HISTORY_LENGTH )
+        nrecs++;
+    
+    if( 0 == tvhist_write() ) {
+        logmsg(LOG_DEBUG, "Successfully updated history XML file.");
+    } else {
+        logmsg(LOG_ERR, "Could NOT update history XML file. Permission problems?");
+    }
+    
+    return 0;
+
+}
+
+/**
+ * Put a formatted version of the history list in the supplied buffer
+ * @param buff Buffer to store history list in
+ * @param maxlen Maximum length of buffer
+ * @return 0 on success, -1 on failure
+ */
+int
+hist_listbuff(char *buff, size_t maxlen) {
+    struct tm result;
+    int sy, sm, sd, sh, smi, ss;
+    int ey, em, ed, eh, emi, es;    
+    static char wday_name[7][4] = {
+        "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"
+    };
+    static char month_name[12][4] = {
+        "Jan","Feb","Mar","Apr","May","Jun",
+        "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+    };
+    
+    char line[256];
+    *buff = '\0';
+    
+    if( 0 == nrecs ) {
+        snprintf(buff,maxlen,"(no history)\n");
+        return 0;
+    }
+
+    for (size_t i = 0; i < nrecs && maxlen > 0 ; ++i) {
+        fromtimestamp(history[i].ts_start, &sy, &sm, &sd, &sh, &smi, &ss);
+        fromtimestamp(history[i].ts_end, &ey, &em, &ed, &eh, &emi, &es);        
+        (void)localtime_r(&history[i].ts_start, &result);        
+        
+        snprintf(line,sizeof(line),
+         "%s %s %02d %02d:%02d "
+         "%-20s"
+         "%-55s"
+         "%-10s\n",  
+         wday_name[result.tm_wday], month_name[sm-1], sd,
+         sh, smi,
+         history[i].title,
+         history[i].filepath,
+         history[i].profile);
+        if( strnlen(line,256) > maxlen ) {
+            return -1;
+        }
+        strncat(buff,line,maxlen);
+        maxlen -= strnlen(line,256);
+        *line='\0';
+    }    
+    if( maxlen > 0 )
+        return 0;
+    else
+        return -1;
+}
+
+int
+hist_list(int fd) {
+    size_t const maxlen=HISTORY_LENGTH*1024;
+    
+    char *buff = calloc(maxlen, sizeof(char));
+    if( NULL == buff ) {
+        return -1;
+    }
+    if( -1 == hist_listbuff(buff, maxlen) ) {
+        return -1;
+    }
+    _writef(fd, buff);
+    free(buff);
+    return 0;
+}
+
+int
+hist_mail(void) {    
+
+    size_t const maxlen=HISTORY_LENGTH*1024;
+
+    char *buffer_plain = calloc(maxlen, sizeof(char));
+    char *buffer_html = calloc(maxlen, sizeof(char));
+
+    if( buffer_plain == NULL || buffer_html == NULL ) {
+        logmsg(LOG_ERR,"Out of memory when trying to allocate buffer! CRITICAL!");
+        exit(1);
+    }
+
+    if( -1 == hist_listbuff(buffer_plain, maxlen-3) ) {
+        free(buffer_plain);
+        free(buffer_html);
+        return -1;
+    }
+
+    strcat(buffer_plain,"\n\n");
+ 
+    snprintf(buffer_html,maxlen-1,"<div>Previous recordings:</div><pre>%s</pre>\n\n",buffer_plain);
+
+    char subject[255];
+    snprintf(subject,255,"List of previous made recordings");
+
+    int ret = sendmail_helper(subject, buffer_plain, buffer_html);
+
+    free(buffer_html);
+    free(buffer_plain);    
+
+    return ret;
+    
+}
+
+/* EOF */
