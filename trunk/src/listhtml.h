7b529a19b2c3fb8c53d82d8903c585c4321294e9
johan162 <johan162@8976e5d5-1644-42d6-8671-f5d7455f965a>
2012-02-27 22:06:36 +0000

    Bumped year in copyright notice

--- trunk/src/listhtml.h
+++ trunk/src/listhtml.h
@@ -1,8 +1,26 @@
-/* 
- * File:   listhtml.h
- * Author: ljp
+/* =========================================================================
+ * File:        LISTHTML.H
+ * Description: Data structures and helper functions used to generate a
+ *              CSS styled table. USed to generate pretty versions of the
+ *              recording and history tables.
+ * Author:      Johan Persson (johan162@gmail.com)
+ * SVN:         $Id$
  *
- * Created on February 27, 2012, 8:41 PM
+ * Copyright (C) 2009-2012 Johan Persson
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>
+ * =========================================================================
  */
 
 #ifndef LISTHTML_H
--- trunk/src/vctrl.c
+++ trunk/src/vctrl.c
@@ -14,7 +14,7 @@
  * Author:      Johan Persson (johan162@gmail.com)
  * SVN:         $Id$
  *
- * Copyright (C) 2009,2010,2011 Johan Persson
+ * Copyright (C) 2009,2010,2011,2012 Johan Persson
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
