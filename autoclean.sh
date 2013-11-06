855d44304819286f972d8d130d9747af64a4c71b
johan162 <johan162@gmail.com>
2013-11-06 13:45:46 +0000

    Updated autoclean.sh to really clean all generated files including all Makefile.in created files

--- autoclean.sh
+++ autoclean.sh
@@ -3,5 +3,6 @@
 # Clean all files that autotools have generated
 # They can be re-created by running bootstrap.sh
 #
-rm -f configure aclocal.m4 ar-lib config.guess config.rpath config.sub depcomp install-sh missing Makefile.in
-rm -rf autom4te.cache
+make maintainer-clean > /dev/null 2>&1
+rm -f configure aclocal.m4 ar-lib config.guess config.rpath config.sub depcomp install-sh missing Makefile.in scripts/pm/Makefile.in scripts/Makefile.in src/*.in src/libsmtpmail/*.in src/tvpowerd/Makefile.in src/libiniparser/*.in src/shell/*.in docs/*.in
+
