5e2967114b4034765b09da98f310e6cf48e16827
johan162 <johan162@8976e5d5-1644-42d6-8671-f5d7455f965a>
2013-11-06 14:06:02 +0000

    Updated autoclean.sh to remove all generated files

--- trunk/autoclean.sh
+++ trunk/autoclean.sh
@@ -4,5 +4,5 @@
 # They can be re-created by running bootstrap.sh
 #
 make maintainer-clean > /dev/null 2>&1
-rm -f configure aclocal.m4 ar-lib config.guess config.rpath config.sub depcomp install-sh missing Makefile.in scripts/pm/Makefile.in scripts/Makefile.in src/*.in src/libsmtpmail/*.in src/tvpowerd/Makefile.in src/libiniparser/*.in src/shell/*.in docs/*.in
+rm -f ChangeLog configure aclocal.m4 ar-lib config.guess config.rpath config.sub depcomp install-sh missing Makefile.in scripts/pm/Makefile.in scripts/Makefile.in src/*.in src/libsmtpmail/*.in src/tvpowerd/Makefile.in src/libiniparser/*.in src/shell/*.in docs/*.in docs/manpages/Makefile.in
 
